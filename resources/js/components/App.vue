be9c0679c48bf4e78256da501f1d8fb6f9f5c408
illupovero <davideluporini1996@gmail.com>
2022-04-05 18:01:04 +0200

    compoent PostList

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header"><h1>Welcome</h1></div>
+    <Header />
 
-          <div class="card-body"><h3>Work in progress</h3></div>
-        </div>
-      </div>
-    </div>
+    <Postlist />
   </div>
 </template>
 
 <script>
+import Header from "./Header.vue";
+import Postlist from "./posts/Postlist.vue";
 name: "App";
+Components: {
+  Header,
+  Postlist,
+}
 </script>
--- resources/js/front.js
+++ resources/js/front.js
@@ -6,4 +6,5 @@ import App from './components/App.vue';
 const root = new Vue({
     el: '#root',
     render: h => h(App),
+
 });
