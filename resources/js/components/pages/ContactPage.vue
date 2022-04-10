d12d4702bb1471878025580994c4fa338ee1879f
illupovero <davideluporini1996@gmail.com>
2022-04-11 00:22:15 +0200

    error in method

---
+++ resources/js/components/pages/ContactPage.vue
@@ -0,0 +1,57 @@
+<template>
+    <section>
+        <h1>Contact us</h1>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input
+                type="email"
+                class="form-control"
+                id="email"
+                v-model="form.email"
+            />
+            <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+            >
+        </div>
+        <div class="form-group">
+            <label for="message">message</label>
+            <textarea
+                class="form-control"
+                id="message"
+                rows="20"
+                v-model="form.message"
+            ></textarea>
+        </div>
+        <div class="d-flex justify-content-end">
+            <button class="btn btn-sm btn-primary" @click="sendForm()">
+                Send
+            </button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "ContactPage",
+    data() {
+        return {
+            form: {
+                email: "",
+                message: "",
+            },
+        };
+    },
+    methods: {
+        sendForm() {
+            axios.post("http://localhost::8000/api/messages", this.form).then(res) => {this.form.email = ''; this.form.message = '';}).catch((err) => {
+
+            }).then(() => {
+
+            });
+        },
+
+    };
+};
+</script>
+
+<style></style>
